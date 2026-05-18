2207bd73b6b07277aa68edb93b3be80def52b508
AbigailNazario <136490258+AbigailNazario@users.noreply.github.com>
2026-05-17 22:53:39 -0300

    ajustando icone para solucionar o PWA

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -101,8 +101,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     links: [
       { rel: "stylesheet", href: appCss },
       { rel: "manifest", href: "/manifest.webmanifest" },
-      { rel: "icon", href: "/icon.png", type: "image/png" },
-      { rel: "apple-touch-icon", href: "/icon.png" },
+      { rel: "icon", href: "/icon-192.png", type: "image/png" },
+      { rel: "apple-touch-icon", href: "/icon-512.png" },
     ],
   }),
   shellComponent: RootShell,
